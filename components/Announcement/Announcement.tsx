8d76d2cf1ad5649787739508b772c65f2ef3c4a8
Heloriel <andradematheus.dev@gmail.com>
2022-09-26 14:19:15 -0300

    Close announcement

--- components/Announcement/Announcement.tsx
+++ components/Announcement/Announcement.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+import { useState } from 'react';
 import {CgClose} from 'react-icons/cg';
 
 type Props = {
@@ -5,14 +7,26 @@ type Props = {
 }
 
 export default function Announcement({message}: Props) {
+  const [isAnnounceVisible, setIsAnnounceVisible] = useState(true);
+
+  function handleClick(){
+    setIsAnnounceVisible(!isAnnounceVisible);
+  }
+
   return (
     <div
-      className="flex w-full h-10 items-center justify-center bg-neutral-800 text-white text-xs px-4"
-    >
+    className={classNames(
+      "flex w-full h-10 items-center justify-center bg-neutral-800 text-white text-xs px-4 transition-all overflow-hidden",
+      {"h-0": !isAnnounceVisible}
+      )}>
       <div className='flex-1 text-center'>
         <span className='-mr-6'>{message}</span>
       </div>
-      <div><a role={'button'}><CgClose size={20} className="leading-none" /></a></div>
+      <div>
+        <a role={'button'} onClick={handleClick}>
+          <CgClose size={20} className="leading-none" />
+        </a>
+      </div>
     </div>
   )
 }
