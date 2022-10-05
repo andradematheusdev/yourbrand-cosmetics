15cdbba5e2bbd9baebd2b2467ffe4047ceffc99c
Heloriel <andradematheus.dev@gmail.com>
2022-10-04 21:47:18 -0300

    Products html

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
 import {AiFillStar} from "react-icons/ai";
+import Button from '../Button/Button';
 
 export default function Product() {
   return (
-    <div className='flex flex-1 flex-col'>
-      <div className='flex justify-end flex-1 md:px-4 lg:px-0'>
-        <div className='w-[100px] h-[100px] relative '>
+    <div className='flex flex-col justify-center gap-y-1'>
+      <div className='flex justify-center flex-1 md:px-4 lg:px-0'>
+        <div className='w-full relative aspect-square'>
           <Image src={"/images/slider/slide1.jpg"} objectFit={'cover'} layout={'fill'} width={"100%"} height={"100%"} alt={"YouCare Shinnee"} />
         </div>
       </div>
-      <h6>Nome</h6>
-      <div className='flex w-full'>
-        <AiFillStar />
-        <AiFillStar />
-        <AiFillStar />
+      <span className='text-center text-base uppercase'>Nome</span>
+      <span className='text-base text-center font-bold'>R$9,90</span>
+      <div className='flex w-full justify-center'>
+        <AiFillStar color='orange' />
+        <AiFillStar color='orange' />
+        <AiFillStar color='orange' />
         <AiFillStar />
         <AiFillStar />
       </div>
+      <Button text='COMPRAR' />
     </div>
   )
 }
--- components/TopSelling/TopSelling.tsx
+++ components/TopSelling/TopSelling.tsx
@@ -3,8 +3,11 @@ import Product from "../Product/Product";
 export default function TopSelling() {
   return (
     <div className="flex flex-col w-full items-center justify-center text-3xl mt-16">
-      <h1>OS QUERIDINHOS</h1>
-      <div className="flex w-full justify-center">
+      <h1 className="mb-8">OS QUERIDINHOS</h1>
+      <div className="
+        grid grid-rows-4 grid-cols-1 md:grid-rows-2 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-1 gap-x-6 gap-y-6 w-full
+        px-4 sm:p-0 sm:w-6/12 md:w-10/12
+      ">
         <Product />
         <Product />
         <Product />
