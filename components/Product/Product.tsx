2b8a49b39417c98339dce91df514678f558f35dc
Heloriel <andradematheus.dev@gmail.com>
2022-11-10 14:44:19 -0300

    Product component props

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -2,16 +2,22 @@ import Image from 'next/image';
 import {AiFillStar} from "react-icons/ai";
 import Button from '../Button/Button';
 
-export default function Product() {
+interface IProductProps{
+  name: string;
+  image: string;
+  price: string;
+}
+
+export default function Product({image,name,price}: IProductProps) {
   return (
     <div className='flex flex-col justify-center gap-y-1'>
       <div className='flex justify-center flex-1 md:px-4 lg:px-0'>
         <div className='w-full relative aspect-square'>
-          <Image src={"/images/slider/slide1.jpg"} objectFit={'cover'} layout={'fill'} width={"100%"} height={"100%"} alt={"YouCare Shinnee"} />
+          <Image src={image} objectFit={'cover'} layout={'fill'} width={"100%"} height={"100%"} alt={"YouCare Shinnee"} />
         </div>
       </div>
-      <span className='text-center text-base uppercase'>Nome</span>
-      <span className='text-base text-center font-bold'>R$9,90</span>
+      <span className='text-center text-base uppercase'>{name}</span>
+      <span className='text-base text-center font-bold'>R${parseFloat(price).toFixed(2)}</span>
       <div className='flex w-full justify-center'>
         <AiFillStar color='orange' />
         <AiFillStar color='orange' />
