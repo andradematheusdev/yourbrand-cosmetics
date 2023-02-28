e4bff2b1fbb6f844d3938401a3fcded95e03a2cd
Heloriel <andradematheus.dev@gmail.com>
2023-02-28 17:47:32 -0300

    Update: carousel structure

--- components/Carousel/CarouselDots/index.tsx
+++ components/Carousel/CarouselDots/index.tsx
@@ -1,12 +1,27 @@
 import {FaCircle, FaRegCircle} from 'react-icons/fa';
 
-export default function CarouselDots() {
+interface ICarouselDotsProps{
+  size: number;
+}
+
+const Dot = () => {
+  return(
+    <input type="radio" name="dots" className='appearance-none inline-block w-4 h-4 rounded-full border-2 border-white checked:bg-white'  />
+  );
+}
+
+export default function CarouselDots({size}: ICarouselDotsProps) {
+  function sliderNavigation(size: number){
+    const dots = [];
+    for (let i = 0; i < size; i++) {
+      dots.push(<Dot />);
+    }
+    return dots; 
+  }
+
   return (
-    <div className="flex w-full gap-x-1 justify-center pb-4 z-10">
-      <FaRegCircle color='#fff' size={12} />
-      <FaCircle color='#fff' size={12} />
-      <FaRegCircle color='#fff' size={12} />
-      <FaRegCircle color='#fff' size={12} />
+    <div className="flex w-full gap-x-1 justify-center">
+      {sliderNavigation(size)}
     </div>
   )
 }
--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -11,9 +11,8 @@ interface IData {
 
 export const Carousel = (props: IData) => {
   return (
-    <div className="flex w-full flex-col h-[600px] max-h-[600px] justify-center relative">
-      <CarouselImage source={props.Data[1].image} />
-      <div className="grid grid-cols-2 grid-rows-1 z-10">
+    <div className="flex w-full flex-col h-[600px] max-h-[600px] justify-between relative">
+      <div className="grid grid-cols-2 grid-rows-1 z-10 h-full items-center">
         <div className="flex w-full">
           <CarouselNavigationLeft />
         </div>
@@ -21,7 +20,10 @@ export const Carousel = (props: IData) => {
           <CarouselNavigationRight />
         </div>
       </div>
-      {/* <CarouselDots /> */}
+      <div className="flex w-full z-20 p-4">
+        <CarouselDots size={props.Data.length} />
+      </div>
+      <CarouselImage source={props.Data[1].image} />
     </div>
   );
 };
