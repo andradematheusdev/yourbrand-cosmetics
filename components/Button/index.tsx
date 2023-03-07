94000fc9490933970ed3eff9b746ec506399ee5b
Heloriel <andradematheus.dev@gmail.com>
2023-03-07 17:34:31 -0300

    Fix: button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,13 +1,14 @@
 interface IButtonProps {
   url?: string;
   text: string;
+  className?: string;
 }
 
-export const Button = ({url = "#", text, ...rest}: IButtonProps) => {
+export const Button = ({url = "#", text, className}: IButtonProps) => {
   return (
     <a
       href={url}
-      className={` flex items-center justify-center border-2 border-black py-3 p-4 font-bold text-sm whitespace-nowrap hover:bg-black hover:text-white transition-colors`}
+      className={` flex items-center justify-center border-2 border-black py-3 p-4 font-bold text-sm whitespace-nowrap hover:bg-black hover:text-white transition-colors ${className}`}
     >
       {text}
     </a>
