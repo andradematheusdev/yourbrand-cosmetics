7b2b676d3bfb44561dc0322aeee97c937094e7ec
Heloriel <andradematheus.dev@gmail.com>
2023-03-07 15:14:20 -0300

    Feat: active page navigation

--- components/MainNavigation/index.tsx
+++ components/MainNavigation/index.tsx
@@ -1,28 +1,32 @@
+import classNames from "classnames";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { SearchPopover } from "../";
 
 export const MainNavigation = () => {
+  const { asPath } = useRouter();
+
   return (
     <nav className="hidden md:flex items-center px-4 uppercase text-sm">
         <ul className="flex h-8 items-center child:px-2 border-r-2 border-neutral-300 pr-1 mr-1">
           <li>
             <Link href={"/"}>
-              <a className="font-bold transition-colors">Home</a>
+              <a className={classNames("transition-colors", {"font-bold": asPath == "/"})}>Home</a>
             </Link>
           </li>
           <li>
             <Link href={"/"}>
-              <a className="transition-colors">Loja</a>
+              <a className={classNames("transition-colors", {"font-bold": asPath == "/loja"})}>Loja</a>
             </Link>
           </li>
           <li>
             <Link href={"/"}>
-              <a className="transition-colors">Linhas</a>
+              <a className={classNames("transition-colors", {"font-bold": asPath == "/linhas"})}>Linhas</a>
             </Link>
           </li>
           <li>
             <Link href={"/"}>
-              <a className="transition-colors">Sobre</a>
+              <a className={classNames("transition-colors", {"font-bold": asPath == "/sobre"})}>Sobre</a>
             </Link>
           </li>
           <li>
@@ -31,10 +35,12 @@ export const MainNavigation = () => {
         </ul>
         <ul className="flex child:px-2">
           <li>
-            <Link href={"#"}><a className="transition-colors">Registrar-se</a></Link>
+            <Link href={"#"}>
+              <a className={classNames("transition-colors", {"font-bold": asPath == "/registro"})}>Registrar-se</a>
+            </Link>
           </li>
           <li>
-            <Link href={"/login"}><a className="transition-colors">Login</a></Link>
+            <Link href={"/login"}><a className={classNames("transition-colors", {"font-bold": asPath == "/login"})}>Login</a></Link>
           </li>
         </ul>
       </nav>
--- components/ProductsCategory/index.tsx
+++ components/ProductsCategory/index.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link"
+
 export const ProductsCategory = () => {
   return (
     <ul className="grid grid-cols-2 text-white px-4">
-      <li>SHINNEE</li>
-      <li>PEARL</li>
-      <li>MAX COLLAGE</li>
-      <li>CLEAR X</li>
-      <li>NATURAL</li>
-      <li>ACQUA</li>
-      <li>MAX ANGEL</li>
-      <li>NANOBLOND</li>
-      <li>PLEX</li>
-      <li>MAX GRAVITY</li>
-      <li>DEEP CLEAR</li>
-      <li>YOU COLORS</li>
-      <li>RADIANCE</li>
+      <li><Link href={"/"}>SHINNEE</Link></li>
+      <li><Link href={"/"}>PEARL</Link></li>
+      <li><Link href={"/"}>MAX COLLAGE</Link></li>
+      <li><Link href={"/"}>CLEAR X</Link></li>
+      <li><Link href={"/"}>NATURAL</Link></li>
+      <li><Link href={"/"}>ACQUA</Link></li>
+      <li><Link href={"/"}>MAX ANGEL</Link></li>
+      <li><Link href={"/"}>NANOBLOND</Link></li>
+      <li><Link href={"/"}>PLEX</Link></li>
+      <li><Link href={"/"}>MAX GRAVITY</Link></li>
+      <li><Link href={"/"}>DEEP CLEAR</Link></li>
+      <li><Link href={"/"}>YOU COLORS</Link></li>
+      <li><Link href={"/"}>RADIANCE</Link></li>
     </ul>
   )
 }
