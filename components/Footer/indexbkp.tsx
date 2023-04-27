70556e237a46e4c9a624634b586cfbf79cdd424e
Heloriel <andradematheus.dev@gmail.com>
2023-04-27 10:59:42 -0300

    Footer overflow fix

--- components/Footer/indexbkp.tsx
+++ components/Footer/indexbkp.tsx
@@ -11,11 +11,11 @@ import {
 export const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="flex flex-col items-center justify-between bg-neutral-900 pt-16 px-4 md:px-16 overflow-x-hidden">
-      <div className="flex justify-center items-center mb-8 max-w-6xl w-full">
+    <footer className="flex flex-col w-full items-center justify-between bg-neutral-900 pt-16">
+      <div className="mb-8">
         <Logo color="#fff" className="scale-150"/>
       </div>
-      <div className="flex flex-col md:flex-row w-full">
+      <div className="flex flex-col md:flex-row w-full px-4 md:px-16">
 
         {/* first col */}
         <div className="flex-1 mb-12 sm:mb-0">
@@ -34,7 +34,7 @@ export const Footer = () => {
         </div>
 
         {/* second col */}
-        <div className="flex-1 mb-8 md:mb-0 md:px-16">
+        <div className="flex-1 mb-8 md:mb-0 place-content-start md:px-16">
           <div className="mb-16 md:mb-8">
             <SectionTitle title="sitemap" className="text-white" />
             <Sitemap />
@@ -51,8 +51,8 @@ export const Footer = () => {
         </div>
 
       </div>
-      <div className="mt-4 w-full max-w-6xl">
-        <div className="flex flex-wrap w-full py-6 items-center justify-center md:justify-between border-t border-neutral-700">
+      <div className="px-4 mt-4 w-full">
+        <div className="flex flex-wrap w-full py-6 items-center justify-center md:justify-between border-t border-neutral-700 px-6">
           <span className="text-white flex-1 flex justify-center md:justify-start">
             <Logo color="#fff" className="mr-2" /> Ⓒ { year }
           </span>
